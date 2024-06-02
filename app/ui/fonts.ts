3eab272b80c6ee8061160c0faed7daa3217e6b45
Saint Dev <st.d447@gmail.com>
2024-06-02 13:20:24 +0530

    chore: Formattting using prettier

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,7 @@
-import {Inter} from 'next/font/google';
-export const inter = Inter({subsets:['latin']});
+import { Inter } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
 import { Lusitana } from 'next/font/google';
-export const lusitana = Lusitana({weight:['400','700'], subsets:['latin']});
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
